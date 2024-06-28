401be77c5a1e243ac01db94ba1d98455182fb865
bárbara miranda <barbaramntmiranda@gmail.com>
2024-06-28 12:11:56 -0300

    add autocomplete functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-
+const cep = document.getElementById('cep');
+const errorMessage = document.getElementById('campo__erro');
 
 async function searchAdress(cep) {
+    errorMessage.innerHTML = '';
     if (cep) {
         try {
             let API = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
@@ -8,14 +10,27 @@ async function searchAdress(cep) {
 
             if (result.erro) {
                 throw new Error('Esse cep não existe!');
-            } else {
-                console.log(result);
-                return result;
             }
+
+            const endereco = document.getElementById('endereco');
+            const bairro = document.getElementById('bairro');
+            const cidade = document.getElementById('cidade');
+            const estado = document.getElementById('estado');
+
+            //preenchimento automático dos dados
+            endereco.value = result.logradouro;
+            bairro.value = result.bairro;
+            cidade.value = result.localidade;
+            estado.value = result.uf;
+
+            return result;
         }
+
         catch (erro) {
             console.log(erro)
+            errorMessage.innerHTML = `<p>Cep inválido.</p>`
         }
     }
 }
-
+//aciona a função quando o usuário clica fora do campo
+cep.addEventListener('focusout', () => searchAdress(cep.value));
